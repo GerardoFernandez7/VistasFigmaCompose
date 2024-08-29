063d3ae2ca2182cdf8b3f3371a9c06c2d4e951ea
hadelacruz <humbertoalexanderdelacruz@gmail.com>
2024-08-29 17:43:30 -0600

    Se agregó el menú desplegable y la vista de splash

--- app/src/main/java/com/joseruiz/project_recipes/screens/Menu.kt
+++ app/src/main/java/com/joseruiz/project_recipes/screens/Menu.kt
@@ -1,2 +1,153 @@
 package com.joseruiz.project_recipes.screens
 
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+
+@Composable
+fun MenuScreen() {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .background(Color(0xFFE57373)) // Fondo rojo
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                // Opciones del menú
+                Column {
+                    Text(
+                        text = "POPULAR RECIPES",
+                        fontSize = 16.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(vertical = 8.dp)
+                    )
+                    Text(
+                        text = "SAVED RECIPES",
+                        fontSize = 16.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(vertical = 8.dp)
+                    )
+                    Text(
+                        text = "SHOPPING LIST",
+                        fontSize = 16.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(vertical = 8.dp)
+                    )
+                    Text(
+                        text = "SETTINGS",
+                        fontSize = 16.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(vertical = 8.dp)
+                    )
+                }
+
+                // Perfil del usuario
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(id = com.joseruiz.project_recipes.R.drawable.receta), // Reemplaza con tu recurso de imagen
+                        contentDescription = "User Avatar",
+                        modifier = Modifier
+                            .size(50.dp)
+                            .clip(CircleShape),
+                        contentScale = ContentScale.Crop
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = "Menú",
+                        fontSize = 16.sp,
+                        color = Color.White
+                    )
+                }
+            }
+        },
+        content = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFFD32F2F)),  // Fondo rojo
+                contentAlignment = Alignment.Center
+            ) {
+                IconButton(
+                    onClick = {
+                        scope.launch { drawerState.open() }
+                    },
+                    modifier = Modifier.align(Alignment.TopEnd).padding(16.dp)
+                ) {
+                    Icon(Icons.Default.Menu, contentDescription = "Open Menu")
+                }
+
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                ) {
+                    Image(
+                        painter = painterResource(id = com.joseruiz.project_recipes.R.drawable.receta),
+                        contentDescription = "Chef Recipe",
+                        modifier = Modifier
+                            .size(200.dp)
+                            .clip(RoundedCornerShape(8.dp)),
+                        contentScale = ContentScale.Crop
+                    )
+                    Spacer(modifier = Modifier.height(26.dp))  // Espacio entre imagen y texto
+                    Text(
+                        text = "Chef Recipes",
+                        style = TextStyle(
+                            color = Color.White,
+                            fontSize = 44.sp,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily.Cursive
+                        )
+                    )
+                }
+
+            }
+
+
+
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+
+        MenuScreen()
+
+}
+
+
